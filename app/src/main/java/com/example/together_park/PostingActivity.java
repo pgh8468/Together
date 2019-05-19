fc750d990744077cc79f750e3d26373ba9ab9f9b
pgh8468 <pgh8468@gmail.com>
2019-05-20 06:51:49 +0900

    지도 옵션

--- app/src/main/java/com/example/together_park/PostingActivity.java
+++ app/src/main/java/com/example/together_park/PostingActivity.java
@@ -1,16 +1,16 @@
 package com.example.together_park;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.UiThread;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
@@ -20,9 +20,6 @@ import com.naver.maps.map.widget.ZoomControlView;
 
 public class PostingActivity extends Activity implements OnMapReadyCallback{
 
-    public NaverMap naverMap;
-    public UiSettings uiSettings = naverMap.getUiSettings();
-    private ZoomControlView ZoomControlView_zoom;
     private MapView mapView;
 
     @Override
@@ -30,9 +27,14 @@ public class PostingActivity extends Activity implements OnMapReadyCallback{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posting);
 
-        ZoomControlView_zoom =findViewById(R.id.ZoomControlView_zoom);
         mapView = findViewById(R.id.map_view);
+        //ZoomControlView_zoom =findViewById(R.id.ZoomControlView_zoom);
         mapView.getMapAsync(this);
+
+        mapView.getMapAsync(naverMap -> {
+            ZoomControlView ZoomControlView_zoom = findViewById(R.id.ZoomControlView_zoom);
+            ZoomControlView_zoom.setMap(naverMap);
+        });
 
         Button Button_save = findViewById(R.id.Button_save);
 
@@ -48,28 +50,56 @@ public class PostingActivity extends Activity implements OnMapReadyCallback{
         });
     }
 
-//    네이버 지도에서 현위치 찾는 메소드
-//    public LocationButtonView(Context context){
-//        public NaverMap getMap
-//    }
-
-
+    @UiThread
     @Override
-    public void onMapReady(@NonNull NaverMap naverMap) { //현위치 잡는 법을 못해서 마크가 제대로 떴는지 모르겠음. 선문대는 확인함.
+    public void onMapReady(@NonNull NaverMap naverMap) {
         Marker marker = new Marker(); //선문대 마크
         Marker marker1 = new Marker();//천안역 마크
         Marker marker2 = new Marker();//아산역
         Marker marker3 = new Marker();//트라팰리스 탕정면사무소
         Marker marker4 = new Marker();//천안터미널 버스정류장으로
         marker.setPosition(new LatLng(36.799218, 127.074920));
+        marker.setOnClickListener(overlay -> { //마크 클릭시 출발지 목적지 선택 가능하도록.
+            Toast.makeText(getApplicationContext(), "선문대학교클릭",Toast.LENGTH_SHORT).show();
+            return true;
+        });
         marker1.setPosition(new LatLng(36.809299, 127.146593));
+        marker1.setOnClickListener(overlay -> { //마크 클릭시 출발지 목적지 선택 가능하도록.
+            Toast.makeText(getApplicationContext(), "천안역클릭",Toast.LENGTH_SHORT).show();
+            return true;
+        });
         marker2.setPosition(new LatLng(36.792171, 127.104496));
+        marker2.setOnClickListener(overlay -> { //마크 클릭시 출발지 목적지 선택 가능하도록.
+            Toast.makeText(getApplicationContext(), "아산역클릭",Toast.LENGTH_SHORT).show();
+            return true;
+        });
         marker3.setPosition(new LatLng(36.798487, 127.060894));
+        marker3.setOnClickListener(overlay -> { //마크 클릭시 출발지 목적지 선택 가능하도록.
+            Toast.makeText(getApplicationContext(), "트라팰리스클릭",Toast.LENGTH_SHORT).show();
+            return true;
+        });
         marker4.setPosition(new LatLng(36.820818, 127.156362));
+        marker4.setOnClickListener(overlay -> { //마크 클릭시 출발지 목적지 선택 가능하도록.
+            Toast.makeText(getApplicationContext(), "천안터미널클릭",Toast.LENGTH_SHORT).show();
+            return true;
+        });
         marker.setMap(naverMap);
-    }
+        marker1.setMap(naverMap);
+        marker2.setMap(naverMap);
+        marker3.setMap(naverMap);
+        marker4.setMap(naverMap);
 
-    public ZoomControlView(@NonNull Context context){
-     return ZoomControlView_zoom;
+        //줌 최대 최소 설정.
+        CameraPosition cameraPosition = naverMap.getCameraPosition();
+        naverMap.setCameraPosition(cameraPosition);
+        naverMap.setMinZoom(5.0);
+        naverMap.setMaxZoom(18.0);
+
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(true);//현재위치 버튼
+        uiSettings.setZoomControlEnabled(true);//줌 확대 축소 버튼
+
+        naverMap.setNightModeEnabled(true);//밤되면 지도 바탕이 어두워지도록.
+        naverMap.setLightness(0.1f);//지도 밝기
     }
 }

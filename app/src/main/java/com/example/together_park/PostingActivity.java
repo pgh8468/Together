b19dc7e8eb5a209592a10c8e00b748f8468b7696
pgh8468 <pgh8468@gmail.com>
2019-05-28 02:48:49 +0900

    05/24

--- app/src/main/java/com/example/together_park/PostingActivity.java
+++ app/src/main/java/com/example/together_park/PostingActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
+import android.support.design.widget.TextInputEditText;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,8 +15,6 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 import com.naver.maps.geometry.LatLng;
-import com.naver.maps.map.CameraAnimation;
-import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
@@ -28,38 +27,41 @@ import com.naver.maps.map.widget.ZoomControlView;
 public class PostingActivity extends Activity implements OnMapReadyCallback {
 
     private MapView mapView;
-    NaverMap naverMap;
-    Marker marker = new Marker();//학교
-    Marker marker1 = new Marker();//천안역 마크
-    Marker marker2 = new Marker();//아산역
-    Marker marker3 = new Marker();//트라팰리스 탕정면사무소
-    Marker marker4 = new Marker();//천안터미널 버스정류장으로
+    //private NaverMap naverMap;
 
-    CameraUpdate cameraUpdate;
+    private Marker marker = new Marker();//학교
+    private Marker marker1 = new Marker();//천안역 마크
+    private Marker marker2 = new Marker();//아산역
+    private Marker marker3 = new Marker();//트라팰리스 탕정면사무소
+    private Marker marker4 = new Marker();//천안터미널 버스정류장으로
 
-    RadioGroup RadioGroup_departure;
-    RadioButton RadioButton_departuresunmoon;
-    RadioButton RadioButton_departureasan;
-    RadioButton RadioButton_departurecheonan;
-    RadioButton RadioButton_departureterminal;
-    RadioButton RadioButton_departuretral;
-    RadioGroup RadioGroup_arrival;
-    RadioButton RadioButton_arrivalsunmoon;
-    RadioButton RadioButton_arrivalasan;
-    RadioButton RadioButton_arrivalcheonan;
-    RadioButton RadioButton_arrivalterminal;
-    RadioButton RadioButton_arrivaltra;
+    private RadioGroup RadioGroup_departure;
+    private RadioButton RadioButton_departuresunmoon;
+    private RadioButton RadioButton_departureasan;
+    private RadioButton RadioButton_departurecheonan;
+    private RadioButton RadioButton_departureterminal;
+    private RadioButton RadioButton_departuretral;
+    private RadioGroup RadioGroup_arrival;
+    private RadioButton RadioButton_arrivalsunmoon;
+    private RadioButton RadioButton_arrivalasan;
+    private RadioButton RadioButton_arrivalcheonan;
+    private RadioButton RadioButton_arrivalterminal;
+    private RadioButton RadioButton_arrivaltra;
+    private RadioGroup RadioGroup_person;
+    private RadioButton RadioButton_one;
+    private RadioButton RadioButton_two;
+    private RadioButton RadioButton_three;
 
-    View Title; //제목 필드값
-    String departure; // 출발지 필드값
-    String arrival; //도착지 필드값
-    int person; //제한인원 필드값
+    private TextInputEditText TextInputEditText; //제목
+    private String Title; //제목 입력하면 여기에
+    private String departure; // 출발지 필드값
+    private String arrival; //도착지 필드값
+    String person; //제한인원 필드값
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posting);
 
-        Title = findViewById(R.id.TextInputEditText);
         RadioGroup_departure = findViewById(R.id.RadioGroup_departure);
         RadioButton_departuresunmoon = findViewById(R.id.RadioButton_departuresunmoon);
         RadioButton_departureasan = findViewById(R.id.RadioButton_departuretasan);
@@ -74,35 +76,38 @@ public class PostingActivity extends Activity implements OnMapReadyCallback {
         RadioButton_arrivalterminal = findViewById(R.id.RadioButton_arrivalterminal);
         RadioButton_arrivaltra = findViewById(R.id.RadioButton_arrivaltra);
 
+        RadioGroup_person = findViewById(R.id.RadioGroup_person);
+        RadioButton_one = findViewById(R.id.RadioButton_one);
+        RadioButton_two = findViewById(R.id.RadioButton_two);
+        RadioButton_three = findViewById(R.id.RadioButton_three);
+
         RadioGroup_departure.setOnCheckedChangeListener(dRadioCheck);
         RadioGroup_arrival.setOnCheckedChangeListener(aRadioCheck);
+        RadioGroup_person.setOnCheckedChangeListener(pRadioCheck);
+
+        TextInputEditText = findViewById(R.id.TextInputEditText);
 
         mapView = findViewById(R.id.map_view);
-        //ZoomControlView_zoom =findViewById(R.id.ZoomControlView_zoom);
         mapView.getMapAsync(this);
 
-        mapView.getMapAsync(naverMap -> {
-            ZoomControlView ZoomControlView_zoom = findViewById(R.id.ZoomControlView_zoom);
-            ZoomControlView_zoom.setMap(naverMap);
-        });
-
         Button Button_save = findViewById(R.id.Button_save);
-        //RadioButton selected
 
         //저장버튼 눌렀을때 제목, 도착지 목적지, 제한인원버튼값을 넘겨줘야함.
         Button_save.setOnClickListener(new View.OnClickListener() { //저장 버튼 눌렀을 때 할 동작
             @Override
             public void onClick(View v) {
+                Title = TextInputEditText.getText().toString(); //버튼을 눌렀을때 데이터가 들어간채로 넘어갈수잇게
                 Intent intent = new Intent(PostingActivity.this, Posting_masterActivity.class);
-                intent.putExtra("departure", "dRadioCheck"); // 출발지 값 넘기기
-                intent.putExtra("arrive", "aRadioCheck"); // 도착지 값 넘기기
-                intent.putExtra("제목", "Title");
-                //intent.putExtra()
 
+                intent.putExtra("Title", Title);
+                intent.putExtra("arrival", arrival);
+                intent.putExtra("departure", departure);
+                intent.putExtra("person", person);
 
-                startActivity(intent);
+                startActivity(intent); //액티비티 이동
 
                 Toast.makeText(getApplicationContext(), "게시 되었습니다.", Toast.LENGTH_SHORT).show(); //게시판으로 넘어갔을때 게시되었습니다. 토스트메세지 띄어줌
+                finish();
             }
         });
 
@@ -111,101 +116,131 @@ public class PostingActivity extends Activity implements OnMapReadyCallback {
     RadioGroup.OnCheckedChangeListener aRadioCheck = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
-          if (i == R.id.RadioButton_arrivalsunmoon){
-              marker.setIcon(MarkerIcons.BLACK);
-              marker.setIconTintColor(Color.YELLOW);
 
-              if (RadioButton_departuresunmoon.isChecked()){
-                 marker.setIconTintColor(Color.RED);
-              }
+            if (i == R.id.RadioButton_arrivalsunmoon){
+                marker.setIcon(MarkerIcons.BLACK);
+                marker.setIconTintColor(Color.YELLOW);
 
-              marker1.setIconTintColor(Color.GREEN);
-              marker2.setIconTintColor(Color.GREEN);
-              marker3.setIconTintColor(Color.GREEN);
-              marker4.setIconTintColor(Color.GREEN);
-          } else if (i == R.id.RadioButton_arrivalasan) {
-              marker2.setIcon(MarkerIcons.BLACK);
-              marker2.setIconTintColor(Color.YELLOW);
+                marker1.setIconTintColor(Color.GREEN);
+                marker2.setIconTintColor(Color.GREEN);
+                marker3.setIconTintColor(Color.GREEN);
+                marker4.setIconTintColor(Color.GREEN);
 
-              marker1.setIconTintColor(Color.GREEN);
-              marker.setIconTintColor(Color.GREEN);
-              marker3.setIconTintColor(Color.GREEN);
-              marker4.setIconTintColor(Color.GREEN);
-          } else if (i == R.id.RadioButton_arrivalcheonan) {
-              marker1.setIcon(MarkerIcons.BLACK);
-              marker1.setIconTintColor(Color.YELLOW);
+                arrival = ((RadioButton)findViewById(R.id.RadioButton_arrivalsunmoon)).getText().toString();
+            } else if (i == R.id.RadioButton_arrivalasan) {
+                marker2.setIcon(MarkerIcons.BLACK);
+                marker2.setIconTintColor(Color.YELLOW);
 
-              marker.setIconTintColor(Color.GREEN);
-              marker2.setIconTintColor(Color.GREEN);
-              marker3.setIconTintColor(Color.GREEN);
-              marker4.setIconTintColor(Color.GREEN);
-          } else if (i == R.id.RadioButton_arrivalterminal) {
-              marker4.setIcon(MarkerIcons.BLACK);
-              marker4.setIconTintColor(Color.YELLOW);
+                marker1.setIconTintColor(Color.GREEN);
+                marker.setIconTintColor(Color.GREEN);
+                marker3.setIconTintColor(Color.GREEN);
+                marker4.setIconTintColor(Color.GREEN);
 
-              marker1.setIconTintColor(Color.GREEN);
-              marker2.setIconTintColor(Color.GREEN);
-              marker3.setIconTintColor(Color.GREEN);
-              marker.setIconTintColor(Color.GREEN);
-          } else if (i == R.id.RadioButton_arrivaltra) {
-              marker3.setIcon(MarkerIcons.BLACK);
-              marker3.setIconTintColor(Color.YELLOW);
+                arrival = ((RadioButton)findViewById(R.id.RadioButton_arrivalasan)).getText().toString();
+            } else if (i == R.id.RadioButton_arrivalcheonan) {
+                marker1.setIcon(MarkerIcons.BLACK);
+                marker1.setIconTintColor(Color.YELLOW);
 
-              marker1.setIconTintColor(Color.GREEN);
-              marker2.setIconTintColor(Color.GREEN);
-              marker.setIconTintColor(Color.GREEN);
-              marker4.setIconTintColor(Color.GREEN);
-          }
+                marker.setIconTintColor(Color.GREEN);
+                marker2.setIconTintColor(Color.GREEN);
+                marker3.setIconTintColor(Color.GREEN);
+                marker4.setIconTintColor(Color.GREEN);
+
+                arrival = ((RadioButton)findViewById(R.id.RadioButton_arrivalcheonan)).getText().toString();
+            } else if (i == R.id.RadioButton_arrivalterminal) {
+                marker4.setIcon(MarkerIcons.BLACK);
+                marker4.setIconTintColor(Color.YELLOW);
+
+                marker1.setIconTintColor(Color.GREEN);
+                marker2.setIconTintColor(Color.GREEN);
+                marker3.setIconTintColor(Color.GREEN);
+                marker.setIconTintColor(Color.GREEN);
+
+                arrival = ((RadioButton)findViewById(R.id.RadioButton_arrivalterminal)).getText().toString();
+            } else if (i == R.id.RadioButton_arrivaltra) {
+                marker3.setIcon(MarkerIcons.BLACK);
+                marker3.setIconTintColor(Color.YELLOW);
+
+                marker1.setIconTintColor(Color.GREEN);
+                marker2.setIconTintColor(Color.GREEN);
+                marker.setIconTintColor(Color.GREEN);
+                marker4.setIconTintColor(Color.GREEN);
+
+                arrival = ((RadioButton)findViewById(R.id.RadioButton_arrivaltra)).getText().toString();
+            }
         }
     };
 
-    RadioGroup.OnCheckedChangeListener dRadioCheck = new RadioGroup.OnCheckedChangeListener() {
+    RadioGroup.OnCheckedChangeListener dRadioCheck;{
+        dRadioCheck = new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {//버튼 클릭에 따라 출발지 마커 색 변경
+                if (checkedId == R.id.RadioButton_departuresunmoon) {
+                    marker.setIcon(MarkerIcons.BLACK);
+                    marker.setIconTintColor(Color.RED);
+
+                    marker1.setIconTintColor(Color.GREEN);
+                    marker2.setIconTintColor(Color.GREEN);
+                    marker3.setIconTintColor(Color.GREEN);
+                    marker4.setIconTintColor(Color.GREEN);
+
+                    departure = ((RadioButton) findViewById(R.id.RadioButton_departuresunmoon)).getText().toString();
+                } else if (checkedId == R.id.RadioButton_departuretasan) {
+                    marker2.setIcon(MarkerIcons.BLACK);
+                    marker2.setIconTintColor(Color.RED);
+
+                    marker1.setIconTintColor(Color.GREEN);
+                    marker.setIconTintColor(Color.GREEN);
+                    marker3.setIconTintColor(Color.GREEN);
+                    marker4.setIconTintColor(Color.GREEN);
+
+                    departure = ((RadioButton) findViewById(R.id.RadioButton_departuretasan)).getText().toString();
+                } else if (checkedId == R.id.RadioButton_departurecheonan) {
+                    marker1.setIcon(MarkerIcons.BLACK);
+                    marker1.setIconTintColor(Color.RED);
+
+                    marker.setIconTintColor(Color.GREEN);
+                    marker2.setIconTintColor(Color.GREEN);
+                    marker3.setIconTintColor(Color.GREEN);
+                    marker4.setIconTintColor(Color.GREEN);
+
+                    departure = ((RadioButton) findViewById(R.id.RadioButton_departurecheonan)).getText().toString();
+                } else if (checkedId == R.id.RadioButton_departureterminal) {
+                    marker4.setIcon(MarkerIcons.BLACK);
+                    marker4.setIconTintColor(Color.RED);
+
+                    marker1.setIconTintColor(Color.GREEN);
+                    marker2.setIconTintColor(Color.GREEN);
+                    marker3.setIconTintColor(Color.GREEN);
+                    marker.setIconTintColor(Color.GREEN);
+
+                    departure = ((RadioButton) findViewById(R.id.RadioButton_departureterminal)).getText().toString();
+                } else if (checkedId == R.id.RadioButton_departuretra) {
+                    marker3.setIcon(MarkerIcons.BLACK);
+                    marker3.setIconTintColor(Color.RED);
+
+                    marker1.setIconTintColor(Color.GREEN);
+                    marker2.setIconTintColor(Color.GREEN);
+                    marker.setIconTintColor(Color.GREEN);
+                    marker4.setIconTintColor(Color.GREEN);
+
+                    departure = ((RadioButton) findViewById(R.id.RadioButton_departuretra)).getText().toString();
+                }
+            }
+        };
+    }
+
+    RadioGroup.OnCheckedChangeListener pRadioCheck = new RadioGroup.OnCheckedChangeListener() {
         @Override
-        public void onCheckedChanged(RadioGroup group, int checkedId) { //버튼 클릭에 따라 출발지 마커 색 변경
-            if (checkedId == R.id.RadioButton_departuresunmoon) {
-                marker.setIcon(MarkerIcons.BLACK);
-                marker.setIconTintColor(Color.RED);
-
-                marker1.setIconTintColor(Color.GREEN);
-                marker2.setIconTintColor(Color.GREEN);
-                marker3.setIconTintColor(Color.GREEN);
-                marker4.setIconTintColor(Color.GREEN);
-
-            } else if (checkedId == R.id.RadioButton_departuretasan) {
-                marker2.setIcon(MarkerIcons.BLACK);
-                marker2.setIconTintColor(Color.RED);
-
-                marker1.setIconTintColor(Color.GREEN);
-                marker.setIconTintColor(Color.GREEN);
-                marker3.setIconTintColor(Color.GREEN);
-                marker4.setIconTintColor(Color.GREEN);
-            } else if (checkedId == R.id.RadioButton_departurecheonan) {
-                marker1.setIcon(MarkerIcons.BLACK);
-                marker1.setIconTintColor(Color.RED);
-
-                marker.setIconTintColor(Color.GREEN);
-                marker2.setIconTintColor(Color.GREEN);
-                marker3.setIconTintColor(Color.GREEN);
-                marker4.setIconTintColor(Color.GREEN);
-            } else if (checkedId == R.id.RadioButton_departureterminal) {
-                marker4.setIcon(MarkerIcons.BLACK);
-                marker4.setIconTintColor(Color.RED);
-
-                marker1.setIconTintColor(Color.GREEN);
-                marker2.setIconTintColor(Color.GREEN);
-                marker3.setIconTintColor(Color.GREEN);
-                marker.setIconTintColor(Color.GREEN);
-            } else if (checkedId == R.id.RadioButton_departuretra) {
-                marker3.setIcon(MarkerIcons.BLACK);
-                marker3.setIconTintColor(Color.RED);
-
-                marker1.setIconTintColor(Color.GREEN);
-                marker2.setIconTintColor(Color.GREEN);
-                marker.setIconTintColor(Color.GREEN);
-                marker4.setIconTintColor(Color.GREEN);
+        public void onCheckedChanged(RadioGroup radioGroup, int i) {
+            if (i == R.id.RadioButton_one) {
+                person = ((RadioButton) findViewById(R.id.RadioButton_one)).getText().toString();
+            } else if (i == R.id.RadioButton_two) {
+                person = ((RadioButton) findViewById(R.id.RadioButton_two)).getText().toString();
+            } else if (i == R.id.RadioButton_three) {
+                person = ((RadioButton) findViewById(R.id.RadioButton_three)).getText().toString();
             }
         }
-
     };
 
     @UiThread
@@ -242,21 +277,6 @@ public class PostingActivity extends Activity implements OnMapReadyCallback {
         marker3.setMap(naverMap);
         marker4.setMap(naverMap);
 
-        cameraUpdate = CameraUpdate.scrollTo(new LatLng(36.799218, 127.074920))
-                .animate(CameraAnimation.Easing);
-        naverMap.moveCamera(cameraUpdate);
-
-        //줌 최대 최소 설정.
-        CameraPosition cameraPosition = naverMap.getCameraPosition();
-        naverMap.setCameraPosition(cameraPosition);
-        naverMap.setMinZoom(5.0);
-        naverMap.setMaxZoom(18.0);
-
-        UiSettings uiSettings = naverMap.getUiSettings();
-        uiSettings.setLocationButtonEnabled(true);//현재위치 버튼
-        uiSettings.setZoomControlEnabled(true);//줌 확대 축소 버튼
-
-        naverMap.setNightModeEnabled(true);//밤되면 지도 바탕이 어두워지도록.
         naverMap.setLightness(0.1f);//지도 밝기
     }
 }
--- app/src/main/java/com/example/together_park/Posting_masterActivity.java
+++ app/src/main/java/com/example/together_park/Posting_masterActivity.java
@@ -1,18 +1,56 @@
 package com.example.together_park;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.naver.maps.map.MapView;
 
 public class Posting_masterActivity extends AppCompatActivity {
 
-
+    private TextView TextView_Title;
+    private TextView TextView_arrival;
+    private TextView TextView_departure;
+    private TextView TextView_person;
+    Button Button_call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posting_master);
-    }
 
+        TextView_Title = findViewById(R.id.TextView_Title);
+        TextView_arrival = findViewById(R.id.TextView_arrival);
+        TextView_departure = findViewById(R.id.TextView_departure);
+        TextView_person = findViewById(R.id.TextView_person);
+        Button_call = findViewById(R.id.Button_call);
+
+        Intent intent = getIntent();
+        Intent dialintent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:010-5573-8468"));
+
+        String Title = intent.getStringExtra("Title");
+        String arrival = intent.getStringExtra("arrival");
+        String departure = intent.getStringExtra("departure");
+        String person = intent.getStringExtra("person");
+
+        TextView_departure.setText(departure);
+        TextView_Title.setText(Title);
+        TextView_arrival.setText(arrival);
+        TextView_person.setText(person);
+
+        //콜벤 전화버튼 누르면 전화화면으로 전환
+        Button_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dialintent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:010-5573-8468"));
+                startActivity(dialintent);
+            }
+        });
+
+        //startActivity(dialintent);
+    }
 }
